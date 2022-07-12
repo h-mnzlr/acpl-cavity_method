5fb328bf3ba74e8c36e5056f4be019512ea1d7ec
h-mnzlr <67161416+h-mnzlr@users.noreply.github.com>
2022-07-12 12:27:19 +0200

    Add anderson model stuff.

--- code/disordered_sys/anderson.py
+++ code/disordered_sys/anderson.py
@@ -7,13 +7,29 @@ import networkx as nx
 
 from numpy.typing import NDArray
 
-def anderson_graph(conn: int = 3, size: int = 2**10) -> nx.Graph:
+def anderson_graph(conn: int = 3, size: int = 2**10, seed=None) -> nx.Graph:
     """Create an infinite dimensional Anderson Graph."""
-    return nx.random_regular_graph(conn, size)
+    if seed is None:
+        seed = np.random.default_rng()
+    return nx.random_regular_graph(conn, size, seed=seed)
 
-def anderson_hamiltonian(conn_graph: nx.Graph, w: float = .3) -> NDArray:
+def anderson_hamiltonian(
+    conn_graph: nx.Graph,
+    w: float = .3,
+) -> NDArray[np.float64]:
     """Create an Anderson Hamiltonian matrix from a graph."""
     adj_matrix = nx.adjacency_matrix(conn_graph)
     anderson_noise = np.random.uniform(low=-w, high=w, size=len(conn_graph))
 
     return -adj_matrix + anderson_noise * np.identity(len(conn_graph))
+
+def anderson_cavity_eqn(
+    samples: NDArray[np.float64] | np.float64,
+    eigv: float,
+    impurity_w: float,
+    epsilon: float,
+    rng: np.random.Generator = np.random.default_rng()
+):
+    """Implements the cavity equation for the Anderson model."""
+    e = rng.uniform(low=-impurity_w, high=impurity_w)
+    return 1j * (eigv - 1j * epsilon - e) + np.sum(1 / samples)
---
+++ code/disordered_sys/methods.py
@@ -0,0 +1,59 @@
+# Heiko Menzler
+# heikogeorg.menzler@stud.uni-goettingen.de
+#
+# Date: 12.07.2022
+
+import numba
+import numpy as np
+
+from numpy.typing import NDArray
+from typing import Callable, Iterable, Any
+
+# Type definitions
+ProgressBar = Callable[[Iterable], Iterable]
+
+Configuration = Any
+ForwardStepper = Callable[[Configuration], Configuration]
+
+Population = NDArray[np.float64]
+PopulationEnsemble = list[Population]
+UpdateEqn = Callable[[Population], Population]
+PopulationStepper = Callable[[Population], Population | None]
+
+identity_iterator = lambda x: (yield from x)
+
+def forward_step_runner(
+    initial_conf: Configuration,
+    forward: ForwardStepper,
+    max_steps: int,
+    pbar: None | ProgressBar = None
+) -> PopulationEnsemble:
+    """Main function of a simple simulation consisting of only forward steps."""
+    if pbar is None:
+        pbar = identity_iterator
+
+    configuration = initial_conf
+    conf_ensemble = []
+
+    for _ in pbar(range(max_steps)):
+        forward(configuration)
+        conf_ensemble.append(configuration)
+    return conf_ensemble
+
+@numba.njit
+def population_step(
+    update_eqn: UpdateEqn,
+    pop: Population,
+    c: int,
+    in_place: bool = True,
+    rng: np.random.Generator = np.random.default_rng()
+) -> Population:
+    """General stepper for the population step method."""
+    if not in_place:
+        pop = pop.copy()
+    n_p = len(pop)
+    population_idxs = rng.integers(low=0, high=n_p - 1, size=c - 1)
+    change_idx = rng.integers(low=0, high=n_p - 1)
+    sample_values = pop.take(population_idxs[:-1])
+    pop[change_idx] = update_eqn(sample_values)
+    return pop
