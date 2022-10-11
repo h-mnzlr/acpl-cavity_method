b406334280a6a2e016ac511f382afbd1fa85e965
h-mnzlr <67161416+h-mnzlr@users.noreply.github.com>
2022-10-11 15:53:37 +0200

    Be correct about population method names.

--- code/disordered_sys/methods.py
+++ code/disordered_sys/methods.py
@@ -7,18 +7,16 @@ from functools import partial
 import numpy as np
 
 from numpy.typing import NDArray
-from typing import Callable, Iterable, Any
+from typing import Callable, Iterable
 
 # Type definitions
 ProgressBar = Callable[[Iterable], Iterable]
+Configuration = NDArray[np.complex64]
 
-Configuration = Any
 ForwardStepper = Callable[[Configuration], Configuration]
 
-Population = NDArray[np.complex64]
-PopulationEnsemble = list[Population]
-UpdateEqn = Callable[[Population], Population]
-PopulationStepper = Callable[[Population], Population | None]
+PopulationEnsemble = list[Configuration]
+UpdateEqn = Callable[[Configuration], Configuration]
 
 identity_iterator = lambda x: (yield from x)
 
@@ -44,25 +42,23 @@ def forward_step_runner(
     return conf_ensemble
 
 def population_step(
-    pop: Population,
+    conf: Configuration,
     update_eqn: UpdateEqn,
     c: int,
     in_place: bool = True,
     rng: np.random.Generator = np.random.default_rng()
-) -> Population:
+) -> Configuration:
     """General stepper for the population step method."""
     if not in_place:
-        pop = pop.copy()
-    n_p = len(pop)
+        conf = conf.copy()
+    n_p = len(conf)
     population_idxs = rng.integers(low=0, high=n_p, size=c - 1)
     change_idx = rng.integers(low=0, high=n_p)
-    sample_values = pop.take(population_idxs[:-1])
-    pop[change_idx] = update_eqn(sample_values)
-    return pop
+    sample_values = conf.take(population_idxs[:-1])
+    conf[change_idx] = update_eqn(sample_values)
+    return conf
 
-def spectral_density(
-    marginal_precisions: list[NDArray[np.float64]]
-) -> list[np.float64]:
+def spectral_density(marginal_precisions: list[NDArray[np.float64]]) -> list[np.float64]:
     """Calculate spectral density from distributions of marginal distributions."""
     spectral_densities = []
     for marginals in marginal_precisions:
@@ -72,22 +68,11 @@ def spectral_density(
     return spectral_densities
 
 def full_population_marginal(
-    initial_pop: Population,
-    update_eqn: UpdateEqn,
-    c: int,
-    rng: np.random.Generator = np.random.default_rng()
+    initial_pop: Configuration, update_eqn: UpdateEqn, c: int, rng: np.random.Generator = np.random.default_rng()
 ) -> PopulationEnsemble:
-    cavity_pop_stepper = partial(
-        population_step, update_eqn=update_eqn, c=c - 1, rng=rng
-    )
-    cavity_pop_ens = forward_step_runner(
-        initial_pop, cavity_pop_stepper, max_steps=100_000
-    )
+    cavity_pop_stepper = partial(population_step, update_eqn=update_eqn, c=c - 1, rng=rng)
+    cavity_pop_ens = forward_step_runner(initial_pop, cavity_pop_stepper, max_steps=100_000)
 
-    marginals_pop_stepper = partial(
-        population_step, update_eqn=update_eqn, c=c, rng=rng
-    )
-    marginals_pop_ens = forward_step_runner(
-        cavity_pop_ens[-1], marginals_pop_stepper, max_steps=12_000
-    )
+    marginals_pop_stepper = partial(population_step, update_eqn=update_eqn, c=c, rng=rng)
+    marginals_pop_ens = forward_step_runner(cavity_pop_ens[-1], marginals_pop_stepper, max_steps=12_000)
     return marginals_pop_ens
